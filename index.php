a6acb2ccb17df202b69d80d2cb81b422ca9479f7
imarcelasol <105014941+imarcelasol@users.noreply.github.com>
2023-04-23 17:56:15 -0300

    subo main actualizado

--- index.php
+++ index.php
@@ -69,21 +69,96 @@
             </section>
             <!-- Cierre Carrousel/ Imagenes del mismo-->
     
-            <!-- Sección La Empresa -->
-            <section>
+           <!-- Seccion empresa-->
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+    <h2 class="w3-wide">LIBCONSTRUCT</h2>
+    <p class="w3-opacity"><i>Cerca de tu hogar siempre</i></p>
+    <p class="w3-justify"><p>Si está pensando en ampliar un espacio, crear uno nuevo o construir, nosotros te acompañamos en cada una de las etapas.
+Somos una empresa dinámica, conformada por un equipo altamente capacitado. Tenemos más de 20 años de experiencia en el rubro y somos el único proveedor que brinda un servicio integral, desde los materiales para la construcción hasta terminaciones y electrodomésticos.
 
-                <h1>Hola Empresa</h1>
+Siempre buscamos aportar una mirada objetiva para que puedas materializar tus ideas de forma más correcta.
+ <!--cierre seccion empresa-->
+ <!--Sucursales-->
+</p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <p>Sucursal Merlo</p>
+        <img src="/img/SucursalMerlo.jpg" class="w3-round w3-margin-bottom"  style="width:150px; height: 150px;">
+      </div>
+      <div class="w3-third">
+        <p>Sucursal Ramos Mejia</p>
+        <img src="/img/Sucursal Ramos Mejia.jpg" class="w3-round w3-margin-bottom"  style="width:150px; height: 150px;">
+      </div>
+      <div class="w3-third">
+        <p>Sucursal Haedo</p>
+        <img src="/img/Sucursal Haedo.jpg" class="w3-round"  style="width:150px; height: 150px;">
+      </div>
+    </div>
+  </div>
+  <!--cierre sucursales-->
 
-            </section>
-            <!-- Cierr Sección La Empresa -->
+  <!-- Seccion ofertas materiales -->
+  <div class="w3-black" id="tour">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">  OFERTAS!</h2>
+      <p class="w3-opacity w3-center"><i>Sólo mes de abril!</i></p><br>
 
-            <!-- Sección materiales -->
-            <section>
+      <ul class="w3-ul w3-border w3-white w3-text-grey">
+        <li class="w3-padding">Baño <span class="w3-tag w3-red w3-margin-left">Merlo</span></li>
+        <li class="w3-padding">Ladrillos <span class="w3-tag w3-red w3-margin-left">Haedo</span></li>
+        <li class="w3-padding">Cerámica <span class="w3-tag w3-red w3-margin-left">Ramos Mejia</span></li>
+      </ul>
 
-                <h1>Hola Materiales</h1>
-
-            </section>
-            <!-- Cierr Sección materiales -->
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="/img/baño_muestra.jpg" alt="Baño" style="width: 245px; height: 200px "  class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Baño</b></p>
+            <p>Combo ferrum andina inodoro y deposito dual<br>$73900</br></p>
+           <!--<button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Comprar</button> -->
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="/img/ladrillos.jpg" alt="Ladrillos" style="width:245px; height: 200px" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Ladrillos</b></p>
+            <p>Ladrillo Comun Merlino 25x5x12cm<br>$32.02 c/u</br></p>
+           <!-- <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Comprar</button> -->
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="/img/ceramica_tiakiki.jpg" alt="Ceramica" style="width: 245px; height: 200px" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Ceramica</b></p>
+            <p>Cerámica Vintage Marrakech Azul 37.5x75 2° Alberdi<br>$2954 m cuadrado</br></p>
+            <!--<button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Comprar</button>-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  <!-- Precios
+  <div id="ticketModal" class="w3-modal">
+    <div class="w3-modal-content w3-animate-top w3-card-4">
+      <header class="w3-container w3-teal w3-center w3-padding-32"> 
+        <span onclick="document.getElementById('ticketModal').style.display='none'" 
+       class="w3-button w3-teal w3-xlarge w3-display-topright">×</span>
+        <h2 class="w3-wide"><i class="fa fa-suitcase w3-margin-right"></i>Precio</h2>
+      </header>
+      <div class="w3-container">
+        <p><label><i class="fa fa-shopping-cart"></i> $73000</label></p>
+        <input class="w3-input w3-border" type="text" placeholder="How many?">
+        <p><label><i class="fa fa-user"></i> Send To</label></p>
+        <input class="w3-input w3-border" type="text" placeholder="Enter email">
+        <button class="w3-button w3-block w3-teal w3-padding-16 w3-section w3-right">PAY <i class="fa fa-check"></i></button>
+        <button class="w3-button w3-red w3-section" onclick="document.getElementById('ticketModal').style.display='none'">Close <i class="fa fa-remove"></i></button>
+        <p class="w3-right">Need <a href="#" class="w3-text-blue">help?</a></p>-->
+      </div>
+    </div>
+  </div>
+    <!--cierre materiales-->
 
             <!-- Seccion Formulario de contacto -->
             <section>
